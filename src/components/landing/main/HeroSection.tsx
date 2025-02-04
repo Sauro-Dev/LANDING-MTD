305d889aaae476708665e15241dd0f2c9af32871
HugoMD <95510299+HugoX2003@users.noreply.github.com>
2025-02-04 16:51:15 -0500

    fix: Ajuste al componente HeroSection
    Ajustamos la imagen para que pueda ser visible y no ocupe demasiado espacio

--- src/components/landing/main/HeroSection.tsx
+++ src/components/landing/main/HeroSection.tsx
@@ -1,17 +1,19 @@
 import { FC } from 'react';
 import bannerImage from '../../../assets/banner/banner principal.png';
 
-const HeroSection: FC = () => (
-    <div className="relative bg-pink-500 min-h-screen overflow-hidden">
-        {/* Banner background image */}
-        <div className="absolute inset-0">
-            <img
-                src={bannerImage}
-                alt="Background Banner"
-                className="w-full h-full object-contain md:object-cover lg:object-fill"
-            />
-        </div>
-    </div>
-);;
+const HeroSection: FC = () => {
+    return (
+        <section className="pt-16">
+            {/* Sección para que el banner no quede detrás del navbar */}
+            <div className="relative w-full aspect-[2000/600] overflow-hidden">
+                <img
+                    src={bannerImage}
+                    alt="Banner principal"
+                    className="w-full h-full object-cover"
+                />
+            </div>
+        </section>
+    );
+};
 
 export default HeroSection;
