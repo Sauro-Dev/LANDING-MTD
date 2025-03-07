a7b19a11c74c1b7e79dae54f5420b94a4bae65da
HugoMD <95510299+HugoX2003@users.noreply.github.com>
2025-03-07 13:21:48 -0500

    feat: responsive a componente HistoryAchievementsSection
    Se agregó responsividad a componente HistoryAchievementsSection, parte de la sección Nosotros, para ajustar el tamaño de letra en pantallas pequeñas

--- src/components/landing/us/HistoryAchievementsSection.tsx
+++ src/components/landing/us/HistoryAchievementsSection.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 
 const HistoryAchievementsSection: FC = () => (
-    <div className="py-16 bg-white">
+    <div className="py-8 md:py-16 bg-white">
         <div className="container mx-auto px-4">
-            <div className="space-y-16">
+            <div className="space-y-8 md:space-y-16">
                 <div>
-                    <h2 className="text-6xl font-[League Spartan] font-bold text-center mb-8 max-w-[900px] mx-auto">
-                        <span className="text-[#ED117F]">HISTORIA REAL:</span>
-                        <span className="text-[#902B74]"> COMENZÓ CON MI SUEÑO PERO SE ESTABLECIÓ CON SU ESTRATEGIA</span>
+                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-[League Spartan] font-bold text-center mb-4 md:mb-8 mx-auto max-w-[90%] md:max-w-[900px]">
+                        <span className="text-[#ED117F] block sm:inline">HISTORIA REAL:</span>
+                        <span className="text-[#902B74] block sm:inline"> COMENZÓ CON MI SUEÑO PERO SE ESTABLECIÓ CON SU ESTRATEGIA</span>
                     </h2>
                 </div>
             </div>
